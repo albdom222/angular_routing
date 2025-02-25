df44e4a51713742cc6e43f8103f6feb981435f2a
albdom222 <albertodl22@gmail.com>
2025-02-25 16:10:24 +0100

    Comentario en product-detail.component

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -12,11 +12,21 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(private _route: ActivatedRoute) {}
 
+  /**
+   *  this._route.params es un OBSERVABLE que emite los parámetros de la ruta cada vez que cambian.
+   *  subscribe(params => {...}) permite recibir los parámetros de la URL cuando el componente se carga o cuando la ruta cambia sin recargar la pagina
+   *  params['id']. Se accede al parámetro llamado id de la URL y se asigna a la variable producto.
+   *  ¿Por qué usamos subscribe?
+   *  El uso de subscribe es importante porque Angular soporta la navegación dentro de una SPA (Single Page Application).
+   *  Esto significa que si el usuario cambia de un producto a otro sin recargar la página, el subscribe detectará el cambio y actualizará el valor sin necesidad de recargar el componente.
+   */
   ngOnInit(): void{
     this._route.params.subscribe(params => {
       //console.log(params['productId']);
       this.producto = params['id'];
     });
   }
+
+
 
 }
